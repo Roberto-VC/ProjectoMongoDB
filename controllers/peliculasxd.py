0b9abe53315042d251042e43f6a66451b02feca1
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2023-02-05 10:47:21 -0600

    estructura
    Co-Authored-By: Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>

--- controllers/peliculasxd.py
+++ controllers/peliculasxd.py
@@ -1,7 +1,11 @@
-from .conn import db
+from flask import request
 
-def get_peliculas():
-    peliculas = []
-    for movie in db.peliculas.find():
-        peliculas.append(movie)
-    return peliculas
+def createPeli(db):
+    id = db.insert_one({
+        "_id": request.json["_id"],
+        "Nombre-de-la-pelicula": request.json["Nombre-de-la-pelicula"],
+        "Año-de-la-pelicula": request.json["Año-de-la-pelicula"],
+        "Genero-de-la-pelicula": request.json["Genero-de-la-pelicula"],
+        "Precio-de-la-entrada-de-la-pelicula": request.json["Precio-de-la-entrada-de-la-pelicula"],
+    })
+    return 'recieved'
--- driver.py
+++ driver.py
@@ -1,44 +1,25 @@
+# Flask Imports
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_cors import CORS
 
-# app.py
-from flask import Flask, request, jsonify
+# Controllers
 import controllers.peliculasxd as pelis
 
-
-countries = [
-    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
-]
-
-def _find_next_id():
-    return max(country["id"] for country in countries) + 1
-
-
-# -------- APP ---------
+# App Config
 app = Flask(__name__)
-@app.route('/')
-def home():
-	return ''
+app.config['MONGO_URI']="mongodb+srv://peliculas:Mongo123@cluster0.dtfcscy.mongodb.net/pueba?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+db = mongo.db.peliculas
 
-def run():
-    app.run(
-		host='0.0.0.0',
-		port=8000
-    )
-    print('API Running in port 8000')
+# Endpoints
+@app.route("/users", methods=["POST"])
+def createPeli():
+    pelis.createPeli(db)
 
-@app.get("/peliculas")
-def get_peliculas():
-    return pelis.get_peliculas()
+@app.route("/users", methods=["GET"])
+def getPeli():
+    return '<h1>recieved'
 
-# Este es un ejemplo que encontre jejej
-@app.post("/countries")
-def add_country():
-    if request.is_json:
-        country = request.get_json()
-        country["id"] = _find_next_id()
-        countries.append(country)
-        return country, 201
-    return {"error": "Request must be JSON"}, 415
-
-run()
+if __name__ == "__main__":
+    app.run(debug = True)
