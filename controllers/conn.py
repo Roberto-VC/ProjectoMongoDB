19e19f25f1bb5c7ff6a61ea43eb683073f550160
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2023-02-06 12:31:52 -0600

    Movies CRUD endpoints

--- controllers/conn.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://peliculas:Mongo123@cluster0.dtfcscy.mongodb.net/?retryWrites=true&w=majority")
-db = client.pueba
--- driver.py
+++ driver.py
@@ -4,22 +4,37 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 
 # Controllers
-import controllers.peliculasxd as pelis
+import controllers.Admin_movies as admin
 
 # App Config
 app = Flask(__name__)
-app.config['MONGO_URI']="mongodb+srv://peliculas:Mongo123@cluster0.dtfcscy.mongodb.net/pueba?retryWrites=true&w=majority"
+app.config['MONGO_URI']="mongodb+srv://peliculas:Mongo123@cluster0.dtfcscy.mongodb.net/Proyecto1?retryWrites=true&w=majority"
 mongo = PyMongo(app)
-db = mongo.db.peliculas
+db = mongo.db
 
-# Endpoints
-@app.route("/users", methods=["POST"])
+# -------------- Endpoints --------------
+
+# Admin movies CRUD
+@app.route("/createMovie", methods=["POST"])
 def createPeli():
-    pelis.createPeli(db)
+    return admin.createPeli(db)
 
-@app.route("/users", methods=["GET"])
+@app.route("/getMovies", methods=["GET"])
 def getPeli():
-    return '<h1>recieved'
+    return admin.getPelis(db)
 
+@app.route("/addActor", methods=["POST"])
+def addActor():
+    return admin.addActor(db)
+
+@app.route("/addGenre", methods=["POST"])
+def addGenre():
+    return admin.addGenre(db)
+
+@app.route("/editMovie", methods=["POST"])
+def editMovie():
+    return admin.editMovie(db)
+
+# -------------- Run API --------------
 if __name__ == "__main__":
     app.run(debug = True)
